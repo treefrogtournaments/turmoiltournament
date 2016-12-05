e2ff5c19708425b7cc1c387741b6d6aeaecf6b3b
Alexander Munger <amunger@databankimx.com>
2016-12-05 13:00:27 -0600

    initial commit

---
+++ test.js
@@ -0,0 +1,175 @@
+{
+      "TransactionType": "Interim Certification",
+      "DocumentTypes": [
+		{
+			"Name": "Client Consent Form for Income Verification and Background Record Release",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Custody/Guardianship Documents",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "EIV Printout",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Family Composition Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "HUD 50058",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Interim Acknowledgement",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Interim Reexaminalion Update Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Marital/Separation Documents",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Notice of Change to Lease and Contract",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Photo IDs",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Release of Information/Privacy Act Notice (HUD-9886)",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "SS Card",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "The Work Number printout",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Third party county verification: DJFS & CSEA",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Third Party Verification Checklist",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "TTP Worksheet",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		}
+      ],
+	  "AdditionalDocuments": [
+		{
+			"Name": "Adoption/FosterCare Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Asset Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Child Care Expense",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Child Support/Alimony Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Contribution Self-Certification",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Contribution Statement",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Employment Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Medical Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Pension/Annuity Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Self-Certification Self-Employment Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Veteran's Benefits Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Worker's Compensation Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		}
+	  ]
+    }
